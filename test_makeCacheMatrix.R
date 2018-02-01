f7d17cbb31c3505f09683c7398ed45f815433097
Shelby Bachman <sbachman18@gmail.com>
2018-02-01 11:51:19 -0500

    added script to test makeCacheMatrix() and solveCache() functions

---
+++ test_makeCacheMatrix.R
@@ -0,0 +1,18 @@
+# define an invertible matrix x
+x <- matrix(c(2,5,2,3), ncol = 2, nrow = 2)
+
+# calculate the special "matrix"
+cacheMatrix <- makeCacheMatrix(x)
+
+# calculate the inverse of the special "matrix", checking first to see if the mean has already been calculated
+inverseMatrix <- cacheSolve(cacheMatrix)
+
+####
+# define a much larger invertible matrix x
+x <- matrix(sample(c(1:10000)), nrow = 100, ncol = 100)
+
+# calculate the special "matrix"
+cacheMatrix <- makeCacheMatrix(x)
+
+# calculate the inverse of the special "matrix", checking first to see if the mean has already been calculated
+inverseMatrix <- cacheSolve(cacheMatrix)
